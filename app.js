6f9d8e7c1bcb60baefddfc144e4e4b4c6abab9df
abhi9065 <110813322+abhi9065@users.noreply.github.com>
2022-09-09 16:10:58 +0530

    submit

---
+++ app.js
@@ -0,0 +1,47 @@
+var initialPrice = document.querySelector("#initial-price");
+var stocksQuantity = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var submitBtn = document.querySelector("#submit-btn");
+var outputBox = document.querySelector("#output-box");
+
+
+submitBtn.addEventListener('click',submitHandler)
+
+function submitHandler(){
+
+    var ip = initialPrice.value;
+    var qty = stocksQuantity.value;
+    var curr = currentPrice.value;
+
+    calculateProfitAndLoss(ip,qty,curr);
+
+
+}
+
+function calculateProfitAndLoss (initial,quantity,current){
+
+    if(initial > current){
+        var loss = (initial-current)*quantity;
+        var lossPercentage = (loss/current)*100;
+
+
+        showOutput(`hey the loss is ${loss} and the percentage is ${lossPercentage}%`)
+    }else if (current > initial){
+        var profit = (current-initial)*quantity;
+        var profitPercentage = (profit/current)*100;
+
+       showOutput(`hey profit is ${profit} and percentage is ${profitPercentage}%`)
+
+    }else{
+
+     showOutput("no pain no gain and no gain no pain")
+
+    }
+
+}
+
+
+function showOutput(message){
+
+    outputBox.innerHTML = message; 
+}
